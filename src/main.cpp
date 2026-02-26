c01ce32c946dbd81669d5ad7ce1f9ea0005282cc
onjamm <azmionjang@gmail.com>
2026-02-26 18:57:55 +0000

    refactord dpad movement in to movement.h and movement.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 
 #include "bn_sprite_items_dot.h"
 #include "bn_sprite_items_square.h"
+#include "movement.h"
 
 // A scaling factor by which to reduce the force applied when orbiting
 // Important for numerical stability
@@ -21,141 +22,125 @@ static constexpr bn::fixed_point ORBITER_START_POSIITON = {0, 0};
 static constexpr bn::fixed_point ORBITER_START_VELOCITY = {0, 5};
 
 /**
- * Moves sprite up/down/left/right based on the d-pad being held.
- * 
- * Dimensions are moved independently, meaning that diagonal motion is faster than moving in one
- * dimension.
- * 
- * @param sprite the sprite to move
- * @param speed the speed at which to move the sprite in each dimension
- */
-void dPadMoveSprite(bn::sprite_ptr& sprite, bn::fixed speed) {
-    bn::fixed dx = 0;
-    bn::fixed dy = 0;
-
-    if(bn::keypad::left_held()) {
-        dx -= speed;
-    }
-    if(bn::keypad::right_held()) {
-        dx += speed;
-    }
-    if(bn::keypad::up_held()) {
-        dy -= speed;
-    }
-    if(bn::keypad::down_held()) {
-        dy += speed;
-    }
-
-    sprite.set_position(sprite.position() + bn::fixed_point(dx, dy));
-}
-
-/**
  * A center of mass to be orbited around. Position is controlled by the player.
  */
-class Center {
-    public:
-        /**
-         * Creates a new center.
-         * 
-         * @param starting_position the initial location of the center
-         * @param mass the mass of the center. The larger the value, the greater the attraction
-         * @param speed the speed at which the center moves when the d-pad is held
-         */
-        Center(bn::fixed_point starting_position, bn::fixed mass, bn::fixed speed) : 
-            _sprite(bn::sprite_items::square.create_sprite(starting_position)),
-            _mass(mass),
-            _speed(speed) {
-        }
+class Center
+{
+public:
+    /**
+     * Creates a new center.
+     *
+     * @param starting_position the initial location of the center
+     * @param mass the mass of the center. The larger the value, the greater the attraction
+     * @param speed the speed at which the center moves when the d-pad is held
+     */
+    Center(bn::fixed_point starting_position, bn::fixed mass, bn::fixed speed) : _sprite(bn::sprite_items::square.create_sprite(starting_position)),
+                                                                                 _mass(mass),
+                                                                                 _speed(speed)
+    {
+    }
 
-        /**
-         * Updates the location of the Center based on player direction with the d-pad.
-         */
-        void update() {
-            dPadMoveSprite(_sprite, _speed);
-        }
+    /**
+     * Updates the location of the Center based on player direction with the d-pad.
+     */
+    void update()
+    {
+        dPadMoveSprite(_sprite, _speed);
+    }
 
-        bn::fixed_point position() {
-            return _sprite.position();
-        }
+    bn::fixed_point position()
+    {
+        return _sprite.position();
+    }
 
-        bn::fixed mass() {
-            return _mass;
-        }
+    bn::fixed mass()
+    {
+        return _mass;
+    }
 
-    private:
-        bn::sprite_ptr _sprite;
-        bn::fixed _mass; // the mass of the center. The larger the value, the greater the attraction
-        bn::fixed _speed; //the speed at which the center moves when the d-pad is held
+private:
+    bn::sprite_ptr _sprite;
+    bn::fixed _mass;  // the mass of the center. The larger the value, the greater the attraction
+    bn::fixed _speed; // the speed at which the center moves when the d-pad is held
 };
 
 /**
  * An object that orbits around a center. Orbits using Hooke's law, as if attached by a 2D spring.
- * The stiffness of the spring in each dimension is center.mass() / FORCE_SCALE. 
+ * The stiffness of the spring in each dimension is center.mass() / FORCE_SCALE.
  */
-class Orbiter {
-    public:
-        /**
-         * Creates an orbiter.
-         * 
-         * @param starting_location the initial location of the Orbiter
-         * @param starting_velocity the initial velocity of the Orbiter, {dx, dy}
-         * @param center the center to orbit around
-         */
-        Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity, Center &center) :
-        _sprite(bn::sprite_items::dot.create_sprite(starting_location)),
-        _velocity(starting_velocity),
-        _center(center) {
-        }
+class Orbiter
+{
+public:
+    /**
+     * Creates an orbiter.
+     *
+     * @param starting_location the initial location of the Orbiter
+     * @param starting_velocity the initial velocity of the Orbiter, {dx, dy}
+     * @param center the center to orbit around
+     */
+    Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity, Center &center) : _sprite(bn::sprite_items::dot.create_sprite(starting_location)),
+                                                                                                    _velocity(starting_velocity),
+                                                                                                    _center(center)
+    {
+    }
 
-        /**
-         * Take a step orbiting around the center.
-         */
-        void update() {
-            // A vector from the Orbiter to the Center
-            bn::fixed_point delta = _center.position() - _sprite.position();
+    /**
+     * Take a step orbiting around the center.
+     */
+    void update()
+    {
+        // A vector from the Orbiter to the Center
+        bn::fixed_point delta = _center.position() - _sprite.position();
 
-            // Scale the vector by the mass and apply a FORCE_SCALE
-            // This is done in two steps instead of having a pre-scaled mass to prevent loss of
-            // fixed point precision
-            bn::fixed_point force = delta * _center.mass();
-            force /= FORCE_SCALE;
+        // Scale the vector by the mass and apply a FORCE_SCALE
+        // This is done in two steps instead of having a pre-scaled mass to prevent loss of
+        // fixed point precision
+        bn::fixed_point force = delta * _center.mass();
+        force /= FORCE_SCALE;
 
-            // Update the current velocity with the force
-            _velocity += force;
+        // Update the current velocity with the force
+        _velocity += force;
 
-            // Update the position by taking a step by the velocity vector
-            _sprite.set_position(_sprite.position() + _velocity);
-        }
+        // Update the position by taking a step by the velocity vector
+        _sprite.set_position(_sprite.position() + _velocity);
+    }
 
-    private:
-        bn::sprite_ptr _sprite;
-        bn::fixed_point _velocity;
-        Center& _center;
+private:
+    bn::sprite_ptr _sprite;
+    bn::fixed_point _velocity;
+    Center &_center;
 };
 
-int main() {
+int main()
+{
     bn::core::init();
 
     Center center = Center({30, 40}, .05, 2);
     bn::vector<Orbiter, MAX_ORBITERS> orbiters = {};
-    
-    while(true) {
+
+    while (true)
+    {
         // Add an orbiter when A is pressed if there's room
-        if(bn::keypad::a_pressed()) {
-            if(orbiters.size() < MAX_ORBITERS) {
+        if (bn::keypad::a_pressed())
+        {
+            if (orbiters.size() < MAX_ORBITERS)
+            {
                 orbiters.push_back(Orbiter(ORBITER_START_POSIITON, ORBITER_START_VELOCITY, center));
             }
         }
 
         // Remove an orbiter when B is pressed if there's at least one
-        if (bn::keypad::b_pressed()) {
-            if(orbiters.size() > 0) {
+        if (bn::keypad::b_pressed())
+        {
+            if (orbiters.size() > 0)
+            {
                 orbiters.pop_back();
             }
         }
 
         center.update();
-        for(Orbiter& orbiter : orbiters) {
+        for (Orbiter &orbiter : orbiters)
+        {
             orbiter.update();
         }
 
