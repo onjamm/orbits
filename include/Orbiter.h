ddbba1896422382e2fba876405aa7127acecf8e3
onjamm <75151498+onjamm@users.noreply.github.com>
2026-03-02 23:27:48 +0000

    Refactored orbiter in to a new header and implementation file, and I didn't forget the include guard!

---
+++ include/Orbiter.h
@@ -0,0 +1,21 @@
+#ifndef ORBITER_H
+#define ORBITER_H
+
+#include <bn_sprite_ptr.h>
+#include <bn_fixed_point.h>
+#include "Center.h"
+
+class Orbiter
+{
+    public:
+        Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity,
+        Center &center);
+
+        void update();
+
+    private:
+        bn::sprite_ptr _sprite;
+        bn::fixed_point _velocity;
+        Center& _center;
+};
+#endif
